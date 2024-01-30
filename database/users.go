781af11edb34320573e9285f14551c77d3db005f
Simon <av_smsuarez@idcontractors.net>
2024-01-30 13:59:16 +0900

    Select user and select users completed

--- database/users.go
+++ database/users.go
@@ -3,6 +3,7 @@ package database
 import (
 	"database/sql"
 	"log"
+	"strconv"
 	"strings"
 
 	"github.com/SimonMora/bikesams_be/models"
@@ -23,7 +24,7 @@ func SearchUser(user string) (models.UserResponse, bool) {
 	}
 	defer Db.Close()
 
-	sentence := "SELECT User_UUID, User_Email, User_FirstName, User_LastName FROM users WHERE User_UUID = '" + user + "'"
+	sentence := "SELECT User_UUID, User_Email, User_FirstName, User_LastName, User_DateUpg FROM users WHERE User_UUID = '" + user + "'"
 
 	result, err = Db.Query(sentence)
 	if err != nil {
@@ -32,7 +33,7 @@ func SearchUser(user string) (models.UserResponse, bool) {
 	}
 
 	result.Next()
-	err = result.Scan(&userDb.UserUUID, &userDb.User_Email, &userDb.User_FirstName, &userDb.User_LastName)
+	err = result.Scan(&userDb.UserUUID, &userDb.User_Email, &userDb.User_FirstName, &userDb.User_LastName, &userDb.User_DateUpg)
 	if err != nil {
 		log.Default().Println("Error parsing the user retrieved from the database.. " + err.Error())
 		return userResp, false
@@ -41,6 +42,7 @@ func SearchUser(user string) (models.UserResponse, bool) {
 		userResp.User_Email = userDb.User_Email.String
 		userResp.User_FirstName = userDb.User_FirstName.String
 		userResp.User_LastName = userDb.User_LastName.String
+		userResp.User_DateUpg = userDb.User_DateUpg.Time
 	}
 
 	return userResp, true
@@ -82,4 +84,64 @@ func UpdateUser(request models.UserRequest, user string) (models.UserResponse, e
 
 	log.Default().Println("User Update sucessfull..")
 	return userResp, nil
+}
+
+func SelectAllUsers(page int) ([]models.UserResponse, error) {
+	log.Default().Println("Start Select All users ..")
+	var users []models.UserResponse
+
+	err := DbConnect()
+	if err != nil {
+		log.Default().Println("Error connecting to the database..")
+		return users, err
+	}
+	defer Db.Close()
+
+	offset := (page * 10) - 10
+
+	sentence := "SELECT * FROM users"
+	countSentence := "SELECT count(*) FROM users"
+
+	if page > 1 {
+		sentence += " OFFSET " + strconv.Itoa(offset)
+	}
+
+	var result *sql.Rows
+	result, err = Db.Query(countSentence)
+	result.Next()
+
+	var records sql.NullInt32
+	err = result.Scan(&records)
+	if err != nil {
+		log.Default().Println("Error retrieving count of users from the database: " + err.Error())
+		return users, err
+	}
+
+	log.Default().Println(sentence) //Only uncomment for debug purposes
+
+	result, err = Db.Query(sentence)
+	if err != nil {
+		log.Default().Println("Error executing sentence in the database.." + err.Error())
+		return users, err
+	}
+
+	for result.Next() {
+		var dbUser models.User
+		user := models.UserResponse{}
+		err = result.Scan(
+			&dbUser.UserUUID, &dbUser.User_Email, &dbUser.User_FirstName,
+			&dbUser.User_LastName, &dbUser.User_Status, &dbUser.User_DateAdd,
+			&dbUser.User_DateUpg,
+		)
+		if err != nil {
+			log.Default().Println("Error parsing users: " + err.Error())
+			return []models.UserResponse{}, err
+		}
+
+		user.ParseUser(dbUser)
+		users = append(users, user)
+	}
+
+	log.Default().Println("Retrieve all users sucessfull..")
+	return users, nil
 }
